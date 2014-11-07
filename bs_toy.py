eda3daefdb60ab644593ddabfe5e707d0a54a808
bisrig <bshepardson@gmail.com>
2014-11-07 02:54:40 +0000

    refactored into function, split summary into constituent parts

--- bs_toy.py
+++ bs_toy.py
@@ -1,30 +1,44 @@
 from bs4 import BeautifulSoup
-import urllib2
+#import urllib2
+
+def get_simple_roster(soup):
+    """
+    Extract the "simple roster" table from a team page.
+    Returns team_roster, a list of dicts, where each list item
+    corresponds to the information for a single player
+    
+    Drill down soup to get to the roster table. Extract the table
+    headings to use as keys for the player dicts. Initialize an empty
+    list to store the information extracted for each player. Loop over
+    each row of the table, producing a player dict with keys corresponding
+    to the table headings and appending to the player list.
+    
+    Parameters:
+    - 'soup': a BeautifulSoup object created from the team page
+    """
+    roster = soup.find('table', id='roster')
+    roster_attributes = []
+    for th in roster.thead.find_all("th"):
+        roster_attributes.append(th['data-stat'])
+
+    team_roster = []
+    for tr in roster.tbody.find_all("tr"):
+        player = {}
+        for k,v in zip(roster_attributes, tr.find_all("td")):
+            if k == 'summary':
+                for s in v.string.split(','):
+                    sep = s.strip().split(' ')
+                    player[sep[1].lower().encode('ascii','ignore')] = float(sep[0])
+            else:
+                player[k] = v.string.encode('ascii','ignore')
+        team_roster.append(player)
+
+    return team_roster
+
+# test driver code
 
 #url = 'http://www.sports-reference.com/cbb/schools/syracuse/2014.html'
 #soup = BeautifulSoup(urllib2.urlopen(url).read())
 soup = BeautifulSoup(open('./test/syr.html').read())
 
-#print soup.prettify()
-
-#body_tag = soup.body
-#page_container = body_tag.find(id="page_container", recursive=False)
-#page_content = page_container.find(id="page_content", recursive=False)
-#all_roster = page_content.find(id="all_roster", recursive=False)
-#all_roster = soup.find(id="all_roster")
-#div_roster = all_roster.find(id="div_roster", recursive=False)
-#roster = div_roster.find(id="roster", recursive=False)
-roster = soup.find('table', id='roster')
-#thead = roster.find("thead", recursive=False)
-roster_attributes = []
-for th in roster.thead.find_all("th"):
-   roster_attributes.append(th['data-stat'])
-
-#tbody = roster.find("tbody", recursive=False)
-
-team_roster = []
-for tr in roster.tbody.find_all("tr"):
-   player = {}
-   for k,v in zip(roster_attributes, tr.find_all("td")):
-      player[k] = v.string
-   team_roster.append(player)
+team_roster = get_simple_roster(soup)
