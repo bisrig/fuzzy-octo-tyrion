41c94af6a06d349a6e5aa56f1ff04b767ffda8b8
bisrig <bshepardson@gmail.com>
2014-11-11 02:02:35 +0000

    refactored table extraction, added function to get mpgs out of per-game table

--- bs_toy.py
+++ bs_toy.py
@@ -1,6 +1,24 @@
 from bs4 import BeautifulSoup
 #import urllib2
 
+def extract_table(table):
+    table_to_extract = soup.find('table', id=table)
+    table_heading = []
+    for th in table_to_extract.thead.find_all("th"):
+        table_heading.append(th['data-stat'])
+
+    extracted_table = []
+    for tr in table_to_extract.tbody.find_all("tr"):
+        player = {}
+        for k,v in zip(table_heading, tr.find_all("td")):
+            if v.string:
+                player[k] = v.string.encode('ascii','ignore')
+            else:
+                player[k] = '0'
+        extracted_table.append(player)
+
+    return extracted_table
+
 def get_simple_roster(soup):
     """
     Extract the "simple roster" table from a team page.
@@ -16,23 +34,27 @@ def get_simple_roster(soup):
     Parameters:
     - 'soup': a BeautifulSoup object created from the team page
     """
-    roster = soup.find('table', id='roster')
-    roster_attributes = []
-    for th in roster.thead.find_all("th"):
-        roster_attributes.append(th['data-stat'])
+    team_roster = extract_table('roster')
+    for player in team_roster:
+        for s in player['summary'].split(','):
+            sep = s.strip().split(' ')
+            player[sep[1].lower().encode('ascii','ignore')] = float(sep[0])
 
-    team_roster = []
-    for tr in roster.tbody.find_all("tr"):
-        player = {}
-        for k,v in zip(roster_attributes, tr.find_all("td")):
-            if k == 'summary':
-                for s in v.string.split(','):
-                    sep = s.strip().split(' ')
-                    player[sep[1].lower().encode('ascii','ignore')] = float(sep[0])
-            else:
-                player[k] = v.string.encode('ascii','ignore')
-        team_roster.append(player)
+    return team_roster
 
+def get_player_list(team_roster):
+    player_list = []
+    for player in team_roster:
+        player_list.append(player['player'])
+    return player_list
+    
+def add_mpg_to_team_roster(team_roster):
+    per_game_table = extract_table('per_game')
+    for player in team_roster:
+        for pg_entry in per_game_table:
+            if pg_entry['player'] == player['player']:
+                player['mpg'] = float(pg_entry['mp_per_g'])
+    
     return team_roster
 
 # test driver code
@@ -41,4 +63,8 @@ def get_simple_roster(soup):
 #soup = BeautifulSoup(urllib2.urlopen(url).read())
 soup = BeautifulSoup(open('./test/syr.html').read())
 
+#extract_table('per_game')
+
 team_roster = get_simple_roster(soup)
+player_list = get_player_list(team_roster)
+team_roster = add_mpg_to_team_roster(team_roster)
